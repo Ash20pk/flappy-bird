12233a167a857aa00c4ee507ce12c20433317d41
Ash@metaschool <ash20pk@gmail.com>
2024-09-21 17:21:46 +0800

    Added Graph Indexer

--- interface/indexers/src/bird-game.ts
+++ interface/indexers/src/bird-game.ts
@@ -1,83 +1,15 @@
+import { BigInt, Address } from "@graphprotocol/graph-ts"
 import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  BatchMetadataUpdate as BatchMetadataUpdateEvent,
-  BirdMinted as BirdMintedEvent,
-  CoordinatorSet as CoordinatorSetEvent,
-  GamePlayed as GamePlayedEvent,
-  GameSetupGenerated as GameSetupGeneratedEvent,
-  LevelUp as LevelUpEvent,
-  MetadataUpdate as MetadataUpdateEvent,
-  OwnershipTransferRequested as OwnershipTransferRequestedEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  PlayerRegistered as PlayerRegisteredEvent,
-  Transfer as TransferEvent
-} from "../generated/BirdGame/BirdGame"
-import {
-  Approval,
-  ApprovalForAll,
-  BatchMetadataUpdate,
   BirdMinted,
-  CoordinatorSet,
   GamePlayed,
-  GameSetupGenerated,
   LevelUp,
-  MetadataUpdate,
-  OwnershipTransferRequested,
-  OwnershipTransferred,
-  PlayerRegistered,
-  Transfer
-} from "../generated/schema"
+  PlayerRegistered
+} from "../generated/BirdGame/BirdGame"
+import { BirdMintedEvent, GamePlayedEvent, LevelUpEvent, PlayerRegisteredEvent, Bird, Game, Player, GameHistory } from "../generated/schema"
 
-export function handleApproval(event: ApprovalEvent): void {
-  let entity = new Approval(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleApprovalForAll(event: ApprovalForAllEvent): void {
-  let entity = new ApprovalForAll(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleBatchMetadataUpdate(
-  event: BatchMetadataUpdateEvent
-): void {
-  let entity = new BatchMetadataUpdate(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._fromTokenId = event.params._fromTokenId
-  entity._toTokenId = event.params._toTokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleBirdMinted(event: BirdMintedEvent): void {
-  let entity = new BirdMinted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+export function handleBirdMinted(event: BirdMinted): void {
+  let entity = new BirdMintedEvent(
+    event.transaction.hash.toHexString() + "-" + event.logIndex.toString()
   )
   entity.owner = event.params.owner
   entity.tokenId = event.params.tokenId
@@ -88,24 +20,30 @@ export function handleBirdMinted(event: BirdMintedEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+
+  let bird = new Bird(event.params.tokenId.toString())
+  let player = Player.load(event.params.owner.toHexString())
+  
+  if (player === null) {
+    player = new Player(event.params.owner.toHexString())
+    player.name = "Unknown"
+    player.highScore = BigInt.fromI32(0)
+    player.xp = BigInt.fromI32(0)
+    player.level = BigInt.fromI32(1)
+    player.save()
+  }
+  
+  bird.owner = player.id
+  bird.highScore = BigInt.fromI32(0)
+  bird.xp = BigInt.fromI32(0)
+  bird.level = BigInt.fromI32(1)
+  bird.imageId = event.params.imageId
+  bird.save()
 }
 
-export function handleCoordinatorSet(event: CoordinatorSetEvent): void {
-  let entity = new CoordinatorSet(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.vrfCoordinator = event.params.vrfCoordinator
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleGamePlayed(event: GamePlayedEvent): void {
-  let entity = new GamePlayed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+export function handleGamePlayed(event: GamePlayed): void {
+  let entity = new GamePlayedEvent(
+    event.transaction.hash.toHexString() + "-" + event.logIndex.toString()
   )
   entity.birdId = event.params.birdId
   entity.gameId = event.params.gameId
@@ -117,25 +55,36 @@ export function handleGamePlayed(event: GamePlayedEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+
+  let gameHistory = new GameHistory(event.transaction.hash.toHexString() + "-" + event.logIndex.toString())
+  let bird = Bird.load(event.params.birdId.toString())
+  let game = Game.load(event.params.gameId.toString())
+  
+  if (bird === null || game === null) return
+  
+  let player = Player.load(bird.owner)
+  if (player === null) return
+  
+  gameHistory.bird = bird.id
+  gameHistory.game = game.id
+  gameHistory.player = player.id
+  gameHistory.score = event.params.score
+  gameHistory.xpEarned = event.params.xpEarned
+  gameHistory.timestamp = event.block.timestamp
+  gameHistory.save()
+  
+  bird.highScore = bird.highScore.gt(event.params.score) ? bird.highScore : event.params.score
+  bird.xp = bird.xp.plus(event.params.xpEarned)
+  bird.save()
+  
+  player.highScore = player.highScore.gt(event.params.score) ? player.highScore : event.params.score
+  player.xp = player.xp.plus(event.params.xpEarned)
+  player.save()
 }
 
-export function handleGameSetupGenerated(event: GameSetupGeneratedEvent): void {
-  let entity = new GameSetupGenerated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.tokenId = event.params.tokenId
-  entity.gameSetupSeed = event.params.gameSetupSeed
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleLevelUp(event: LevelUpEvent): void {
-  let entity = new LevelUp(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+export function handleLevelUp(event: LevelUp): void {
+  let entity = new LevelUpEvent(
+    event.transaction.hash.toHexString() + "-" + event.logIndex.toString()
   )
   entity.birdId = event.params.birdId
   entity.newLevel = event.params.newLevel
@@ -145,56 +94,23 @@ export function handleLevelUp(event: LevelUpEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
+
+  let bird = Bird.load(event.params.birdId.toString())
+  if (bird === null) return
+  
+  bird.level = event.params.newLevel
+  bird.save()
+  
+  let player = Player.load(bird.owner)
+  if (player === null) return
+  
+  player.level = event.params.newLevel
+  player.save()
 }
 
-export function handleMetadataUpdate(event: MetadataUpdateEvent): void {
-  let entity = new MetadataUpdate(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity._tokenId = event.params._tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferRequested(
-  event: OwnershipTransferRequestedEvent
-): void {
-  let entity = new OwnershipTransferRequested(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handleOwnershipTransferred(
-  event: OwnershipTransferredEvent
-): void {
-  let entity = new OwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
-}
-
-export function handlePlayerRegistered(event: PlayerRegisteredEvent): void {
-  let entity = new PlayerRegistered(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
+export function handlePlayerRegistered(event: PlayerRegistered): void {
+  let entity = new PlayerRegisteredEvent(
+    event.transaction.hash.toHexString() + "-" + event.logIndex.toString()
   )
   entity.playerAddress = event.params.playerAddress
   entity.name = event.params.name
@@ -204,19 +120,11 @@ export function handlePlayerRegistered(event: PlayerRegisteredEvent): void {
   entity.transactionHash = event.transaction.hash
 
   entity.save()
-}
 
-export function handleTransfer(event: TransferEvent): void {
-  let entity = new Transfer(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
-
-  entity.save()
+  let player = new Player(event.params.playerAddress.toHexString())
+  player.name = event.params.name
+  player.highScore = BigInt.fromI32(0)
+  player.xp = BigInt.fromI32(0)
+  player.level = BigInt.fromI32(1)
+  player.save()
 }
